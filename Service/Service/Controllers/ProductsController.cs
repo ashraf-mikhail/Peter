68e6084d4602ed803dcd71f842cc44c12bb84c0c
peterfarouk <peter_farouk@outlook.com>
2017-03-05 21:58:11 +0200

    remove the unused "using"

--- Service/Service/Controllers/ProductsController.cs
+++ Service/Service/Controllers/ProductsController.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 using Bussiness;
 using DataAccess;
 using Domain;
-using log4net;
 
 namespace Service.Controllers
 {
--- Service/Service/Logging/MyProjectTracer.cs
+++ Service/Service/Logging/MyProjectTracer.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Web;
 using System.Web.Http.Tracing;
 using log4net;
 
