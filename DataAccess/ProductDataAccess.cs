c197f371dae87e2927e46a972f2fb17a75a190d3
peterfarouk <peter_farouk@outlook.com>
2017-03-11 16:01:39 +0200

    Remove unused code

--- DataAccess/ProductDataAccess.cs
+++ DataAccess/ProductDataAccess.cs
@@ -16,36 +16,9 @@ namespace DataAccess
             List<Product> products = new List<Product>();
 
             XmlDocument xmlDoc = new XmlDocument();
-            //XDocument.Load(Server.MapPath("/App_Data/PageData.xml"));
-            //System.Web.Hosting.HostingEnvironment.MapPath(@"~/App_Data/yourXmlFile.xml")
-
-            string assemblyFile = (
-                new System.Uri(Assembly.GetExecutingAssembly().CodeBase)
-                ).AbsolutePath;
-
-            var xx = Directory.GetCurrentDirectory();
-            //var yy = System.Web.Hosting.HostingEnvironment.MapPath("~/ProductsData.xml");
-            //var zz = System.Web.HttpContext.Current.Server.MapPath("~/ProductsData.xml");
-
-            
-
-            string startupPath = AppDomain.CurrentDomain.RelativeSearchPath;
-
-            var xxx = System.Web.HttpContext.Current.Request.PhysicalApplicationPath;
-            var xxx1 = System.Web.HttpContext.Current.Request.ApplicationPath;
-            var xxx2 = System.Web.HttpContext.Current.Request.FilePath;
-            var xxx3 = System.Web.HttpContext.Current.Request.Path;
-            var xxx4 = System.Web.HttpContext.Current.Request.PhysicalPath;
-            var xxx5 = System.Web.HttpContext.Current.Request.CurrentExecutionFilePath;
-
-            //xmlDoc.Load(@"~/DataAccess/ProductsData.xml");
-
             var appDomain = System.AppDomain.CurrentDomain;
-            var basePath = appDomain.RelativeSearchPath ?? appDomain.BaseDirectory;
-            Path.Combine(basePath, "Templates", "NotificatioEmail.cshtml");
-
             xmlDoc.Load(appDomain.BaseDirectory + "/Data/ProductsData.xml");
-            //xmlDoc.Load("D:\\New folder\\ashraf\\e-commerce\\DataAccess\\ProductsData.xml");
+            
             XmlNodeList nodeList = xmlDoc.DocumentElement.SelectNodes("product");
             foreach (XmlNode node in nodeList)
             {
